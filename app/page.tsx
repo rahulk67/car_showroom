1196bdcb0c64ba00e9accadd25822a234ec99953
rahulk67 <72201681+rahulk67@users.noreply.github.com>
2024-04-16 22:52:33 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,136 @@
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
+import { fuels, manufacturers, yearsOfProduction } from "@/constants";
+import { fetchCars } from "@/utils";
+import Image from "next/image";
+
+export default async  function Home({searchParams}) {
+  const allCars =  await  fetchCars({
+    manufacturer: searchParams.manufaturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuels || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+
+  });
+
+  const isDataEmpty = !Array.isArray(allCars)  || allCars.length < 1 || !allCars;
+
+
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">
+            Car Catalogue
+
+          </h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        
+        <div className="home__filters">
+          <SearchBar />
+          <div
+            className="home__filters-container">
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
+          </div>
+
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+
+              <CarCard car={car}/>
+              
+              ))}
+            </div>  
+
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+
+
+          </section>
+        ):
+        (
+          <div className="home__error-container">
+            <h2 className="text-black text-x1 font-bold">
+              oops no results
+              <p>{allCars?.message}</p>
+            </h2>
+          </div>
+        )}
+
+
+
+
+
+      </div>
+
+    </main>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
